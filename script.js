7cd17831c4a5bde5eaa59ce441f5c637d2940652
nishustrives07 <nishustrives07@gmail.com>
2025-07-19 23:50:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const balanceEl = document.getElementById("balance");
 const incomeAmountEl = document.getElementById("income-amount");
-const expenseAmountEl = document.getElementById("expense-amount");
+const expenseAmountEl = document.getElementById("expenses-amount");
 const transactionListEl = document.getElementById("transaction-list");
 const transactionFormEl = document.getElementById("transaction-form");
 const descriptionEl = document.getElementById("description");
