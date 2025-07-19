780623c4f3bdf05a42c3c298bccd027c6c18d06f
nishustrives07 <nishustrives07@gmail.com>
2025-07-19 23:59:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,86 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SpendWise</title>  
-    <link rel="stylesheet" href="style.css"> 
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
-        <h1>SpendWise</h1>
-        <div class="balance-container">
-            <h2>Your Balance</h2>
-            <h1 id="balance">Rs 0.00</h1>
-            <div class="summary">
-                <div class="income">
-                    <h3>Income</h3>
-                    <p id="income-amount">Rs 0.00</p>
-                </div>
-                <div class="expenses">
-                    <h3>Expenses</h3>
-                    <p id="expenses-amount">₹ 0.00</p>
-                </div>
-            </div>
+const balanceEl = document.getElementById("balance");
+const incomeAmountEl = document.getElementById("income-amount");
+const expenseAmountEl = document.getElementById("expenses-amount");
+const transactionListEl = document.getElementById("transaction-list");
+const transactionFormEl = document.getElementById("transaction-form");
+const descriptionEl = document.getElementById("description");
+const amountEl = document.getElementById("amount");
 
-        </div>
-        <div class="main-content">
-            <div class="transaction-container">
-                <h2>Transactions</h2>
-                <ul id="transaction-list">
-                    <!--TRANSACTIONS WILL BE INSERTED HERE THROUGH JS-->
+let transactions=JSON.parse(localStorage.getItem("transactions")) || [];
+transactionFormEl.addEventListener("submit", addTransaction);
+function addTransaction(e){
+    e.preventDefault();
+    const description=descriptionEl.value.trim();
+    const amount=parseFloat(amountEl.value);
+    console.log(typeof amount);
+    transactions.push({
+        id:Date.now(),
+        description,
+        amount
+    })
+    localStorage.setItem("transactions", JSON.stringify(transactions))
+    updateTransactionList()
+    updateSummary()
+    transactionFormEl.reset()
 
-                </ul>
-            </div>
-            <div class="form-container">
-                <h2>Add Transaction</h2>
+}
+function updateTransactionList(){
+    transactionListEl.innerHTML=""
+    const sortedTransactions=[...transactions].reverse()
+    sortedTransactions.forEach((transaction) => {
+    const transactionEl = createTransactionElement(transaction);
+    transactionListEl.appendChild(transactionEl);
+  });
+}
+function createTransactionElement(transaction){
+const li=document.createElement("li")
+li.classList.add("transaction")
+li.classList.add(transaction.amount>0?"income":"expense")
 
-                <form id="transaction-form">
-                     <div class="form-group">
-                        <label for="description">Description</label>
-                        <input type="text" id="description" placeholder="Enter Description..." required>
-                     </div>
-                     <div class="form-group">
-                        <label for="amount">Amount</label>
-                        <input type="number" id="amount" placeholder="Enter Amount..." required>
-                        <small>Use negative (-) for expenses</small>
-                     </div>
-                     <button type="submit">Add Transaction</button>
-                </form>
-            </div>
-        </div>
-    </div>
-    <script src="script.js"></script>
-</body>
-</html>
+//update the amount formatting
+li.innerHTML=`
+<span>₹ ${transaction.description}</span>
+<span>₹ ${transaction.amount}
+<button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
+</span>
+`;
+return li 
+}
+function updateSummary(){
+    const balance=transactions.reduce((acc,transactions)=>acc+transactions.amount,0)
+    const income=transactions.filter(transaction=> transaction.amount>0)
+    .reduce((acc, transaction)=>acc+transaction.amount,0)
+
+     const expenses = transactions
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => acc + transaction.amount, 0);
+
+    //update ui=> todo:fix the formatting
+     balanceEl.textContent = formatCurrency(balance);
+  incomeAmountEl.textContent = formatCurrency(income);
+  expenseAmountEl.textContent = formatCurrency(expenses);
+
+}
+function formatCurrency(number) {
+  return new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "INR",
+  }).format(number);
+}
+
+function removeTransaction(id) {
+  // filter out the one we wanted to delete
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+
+
+  updateTransactionList();
+  updateSummary();
+}
+
+//initial render
+updateTransactionList();
+updateSummary();
+
+
